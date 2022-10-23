0975642946384e2e705bb5511ae14bf65b69d008
allantrexitstop <76573039+Allan-Tran@users.noreply.github.com>
2022-10-23 01:51:06 -0700

    lol

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 import express from'express';
 import twilio from 'twilio';
+import bodyParser from 'body-parser';
 const { MessagingResponse } = twilio.twiml;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/sms', (req, res) => {
-    
+    // If it is not a number (i.e tags) then we look for recipes
+    // Call the Reciepe.getRecipes
+    // Return list of reciepes
+    console.log(req.body.Body)
+    // Ask for Zip Code
+
+    // else we look for indgredients
+    // Call Reciepe.getIngredients
+    // return a list of locations and their prices
+
   const twiml = new MessagingResponse();
 
   twiml.message('The Robots are coming! Head for the hills!');
