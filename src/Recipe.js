d4ebc2fe36a7954634cbc4c762c5234641a47209
allantrexitstop <76573039+Allan-Tran@users.noreply.github.com>
2022-10-22 22:41:18 -0700

    implemented get ingredients

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,14 @@
 import XMLHttpRequest from 'xhr2';
+import fetch from 'node-fetch';
 
-const apiKey = '&apiKey=d6b702b943fe427884ffdcb4cc7bbf17'
+const apiKey = 'apiKey=d6b702b943fe427884ffdcb4cc7bbf17'
 const map = new Map();
+const ingredients = [];
+const url = [];
 
 function getRecipes(tags) {
   let searchString = 'https://api.spoonacular.com/recipes/complexSearch?';
-  searchString += 'query=' + tags + apiKey;
+  searchString += 'query=' + tags + '&' + apiKey;
   const xhr = new XMLHttpRequest();
   xhr.open('GET', searchString, true);
   xhr.setRequestHeader('Accept', 'application/json');
@@ -16,6 +19,7 @@ function getRecipes(tags) {
         // This needs to be changed to message the twilio thingy
         console.log((i + 1) + ") " + res.results[i]['title'])
         var id = res.results[i]['id'];
+        console.log(id);
         map.set(i + 1, id);
       }
     }
@@ -26,4 +30,22 @@ function getRecipes(tags) {
   // .then((data) => console.log(data));
 }
 
+function getIngredients(id) {
+  let searchString = 'https://api.spoonacular.com/recipes/' + id + '/information';
+  // Append api key
+  searchString += '?' + apiKey + '&includeNutrition=false';
+  console.log(searchString);
+
+  fetch(searchString)
+  .then((response) => response.json())
+  .then((data) => {
+    for (var i = 0; i < data['extendedIngredients'].length; i++) {
+          ingredients.push(data['extendedIngredients'][i].name);
+          // This needs to be changed to message the twilio thingy
+        }
+        url.push(data['sourceUrl']);
+  });
+}
+
 getRecipes(process.argv[2])
+getIngredients(657003);
