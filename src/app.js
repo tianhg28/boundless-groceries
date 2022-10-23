92e8ed0e873d03e4ea5a1cf35f48b7d109da33a4
allantrexitstop <76573039+Allan-Tran@users.noreply.github.com>
2022-10-23 01:07:36 -0700

    fixed auth issue

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-const accountSid = 'ACb0f3b0c2c9a44c07957da94250c84e39';
-const authToken = '45cb0e5cdd2887d24e58f2cecdf76354';
+import * as dotenv from 'dotenv';
+dotenv.config({path: '../.env'});
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 import twilio from 'twilio';
 import * as ReciepeFunctions from './Recipe.js'
 const client = twilio(accountSid, authToken);
@@ -8,6 +10,6 @@ client.messages
   .create({
      body: 'What do you want to eat?',
      from: '+13802104609',
-     to: '+12532493440'
+     to: '+12066980855'
    })
   .then(message => console.log(message.sid));
