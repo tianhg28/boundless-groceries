dad8e103ab2a356e89f25f99bee450fe662b98e7
Yafqa <yafqak@uw.edu>
2022-10-22 20:42:34 -0700

    Created Recipes.js, which outputs recipe names to console given tags

---
+++ src/Recipe.js
@@ -0,0 +1,30 @@
+import XMLHttpRequest from 'xhr2';
+
+const apiKey = '&apiKey=d6b702b943fe427884ffdcb4cc7bbf17'
+const map = new Map();
+
+function getRecipes(tags) {
+  let searchString = 'https://api.spoonacular.com/recipes/complexSearch?';
+  searchString += 'query=' + tags + apiKey;
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', searchString, false);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      const res = JSON.parse(xhr.responseText);
+      console.log(res);
+      for (var i = 0; i < res.results.length; i++) {
+        // This needs to be changed to message the twilio thingy
+        console.log((i + 1) + ") " + res.results[i]['title'])
+        var id = res.results[i]['id'];
+        map.set(i + 1, id);
+      }
+    }
+  };
+  xhr.send(null);
+  // fetch(searchString)
+  // .then((response) => response.json())
+  // .then((data) => console.log(data));
+}
+
+getRecipes('chocolate')
