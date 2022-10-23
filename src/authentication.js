03292fafd575d8e5886a5e4497a3da792d5087ee
Jerry Liu <liuje@Jerrys-MacBook-Pro.local>
2022-10-22 19:50:08 -0700

    fixed authentication issues

--- src/authentication.js
+++ src/authentication.js
@@ -1,15 +1,23 @@
+import { Buffer } from 'node:buffer';
+
 async function getAccessToken() {
     let url = "https://api.kroger.com/v1/connect/oauth2/token";
-    let client_id = "boundlessgroceries-54a8f2bbd34a77ef33396a66c8801cf8401111886519534563";
-    let client_secret = "P-R3POo8eFhbSE4sjI1XZkB-TCasiwwGvTdVt-wc";
+    let clientId = "boundlessgroceries-54a8f2bbd34a77ef33396a66c8801cf8401111886519534563";
+    let clientSecret = "P-R3POo8eFhbSE4sjI1XZkB-TCasiwwGvTdVt-wc";
+
+    const encoded = Buffer.from(`${clientId}:${clientSecret}`, `ascii`);
+    // ClientId and clientSecret must be encoded
+    const authorization = "Basic " + encoded.toString("base64");
+  
     let response = await fetch(url, {
         method: "POST",
         cache: "no-cache",
         headers: {
-          "Authorization": "Basic {{base64(" + client_id  + ":" + client_secret + "client_secret)}}",
+          //"Authorization": "Basic {{base64(" + client_id  + ":" + client_secret + ")}}",
+          "Authorization": authorization,
           "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: "grant_type=client_credentials&scope={{N/A}}"
+        body: "grant_type=client_credentials&scope="
       });
     return response.json();
 }
