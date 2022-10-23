cca54003c94c03bb297d7ad4ee0dc228cf9cebb9
Jerry Liu <liuje@Jerrys-MacBook-Pro.local>
2022-10-23 03:15:58 -0700

    changed get Recipe to use fetch

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,28 +3,20 @@ import fetch from 'node-fetch';
 
 const apiKey = 'apiKey=79e72bb78e0a4ea09a550126ed3a0279'
 const ingredients = [];
-const recipes = [];
 const url = [];
 
-export function getRecipes(tags) {
+export async function getRecipes(tags) {
   let searchString = 'https://api.spoonacular.com/recipes/complexSearch?';
   searchString += 'query=' + tags + '&' + apiKey;
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', searchString, true);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      const res = JSON.parse(xhr.responseText);
-      for (var i = 0; i < res.results.length; i++) {
-        // This needs to be changed to message the twilio thingy
-        console.log((i + 1) + ") " + res.results[i]['title'])
-        var id = res.results[i]['id'];
-        recipes.push(id);
-      }
+  let response = await fetch(searchString, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
     }
-  };
-  xhr.send(null);
-  return recipes;
+  });
+
+  let json = await response.json();
+  return json;
   // fetch(searchString)
   // .then((response) => response.json())
   // .then((data) => console.log(data));
